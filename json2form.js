b4d13e131ecc161e8d70aa2e038cbe833501f4e1
Jacob <danielsock@gmail.com>
2022-06-24 11:19:32 -0700

    Added class and innerHTML support

--- json2form.js
+++ json2form.js
@@ -1,14 +1,26 @@
-function json2form(json) {
+
+function json2form(json, form) {
 	// parse our JSON data
 	let data = JSON.parse(json);
 	// grab each element in the json object
 	for (var i in data) {
 		// create our element type
 		var e = document.createElement(data[i].type);
+		// if innerText Exists add it
+		console.log(data[i]);
+		if (data[i].innerText) {
+			e.innerText = data[i].innerText;
+		}
 		// grab our attributes
 		for (var a in data[i].attributes) {
 			e.setAttribute(a, data[i].attributes[a]);
 		}
+		// grab our classes
+		for (var c in data[i].class) {
+			console.log(data[i].class);
+			e.classList.add(data[i].class[c]);
+		}
 		form.appendChild(e);
 	}
 }
+
